361dd7a97dabd6d504273358d35122528e2b7f8f
3lvintan <elvin@outlook.sg>
2015-09-06 09:10:40 +0800

    Initial Commit

---
+++ Algorthythm/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Algorthythm
+//
+//  Created by IDA-GDS-PDD on 6/9/15.
+//  Copyright (c) 2015 Algomized. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
