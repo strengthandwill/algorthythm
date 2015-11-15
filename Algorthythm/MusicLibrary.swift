5b96f7345523dad0ad68d80a08e4e670d2fe066a
Poh Kah Kong <kahkong87mail@gmail.com>
2015-11-15 15:00:10 +0800

    Update images and colors

--- Algorthythm/MusicLibrary.swift
+++ Algorthythm/MusicLibrary.swift
@@ -13,49 +13,49 @@ struct MusicLibrary {
         [
             "title": "Rise and Shine",
             "description": "Get your morning going by singing along to these classic tracks as you hit the shower bright and early!",
-            "icon": "coffee.pdf",
-            "largeIcon": "coffee_large.pdf",
-            "backgroundColor": ["red": 255, "green": 204, "blue": 51, "alpha": 1.0],
+            "icon": "camera.png",
+            "largeIcon": "camera_large.png",
+            "backgroundColor": ["red": 21, "green": 208, "blue": 130, "alpha": 1.0],
             "artists": ["American Authors", "Vacationer", "Matt and Kim", "MGMT", "Echosmith", "Tokyo Police Club", "La Femme"]
         ],
         [
             "title": "Runner's Rampage",
             "description": "Hit the track hard and get in beast mode with everything from dance tracks to classic hip hop. All the right fuel to motivate you to push your limits.",
-            "icon": "running.pdf",
-            "largeIcon": "running_large.pdf",
-            "backgroundColor": ["red": 255, "green": 102, "blue": 51, "alpha": 1.0],
+            "icon": "bird.png",
+            "largeIcon": "bird_large.png",
+            "backgroundColor": ["red": 247, "green": 203, "blue": 0, "alpha": 1.0],
             "artists": ["Avicii", "Calvin Harris", "Pitbull", "Iggy Azalea", "Rita Ora", "Drake", "Lil Wayne"]
         ],
         [
             "title": "Joy Ride",
             "description": "Let this eclectic playlist take you wherever your heart desires. Cruise along in style to these energetic beats.",
-            "icon": "helmet.pdf",
-            "largeIcon": "helmet_large.pdf",
-            "backgroundColor": ["red": 153, "green": 102, "blue": 204, "alpha": 1.0],
+            "icon": "fan.png",
+            "largeIcon": "fan_large.png",
+            "backgroundColor": ["red": 241, "green": 99, "blue": 71, "alpha": 1.0],
             "artists": ["Afrojack", "Kid Cudi", "Daft Punk", "Icona Pop", "Gesaffelstien", "Roksnoix", "deadmau5"]
         ],
         [
             "title": "In The Zone",
             "description": "Keep calm and focus. Shut out the noise around you and grind away with some mind sharpening instrumental tunes.",
-            "icon": "laptop.pdf",
-            "largeIcon": "laptop_large.pdf",
-            "backgroundColor": ["red": 51, "green": 153, "blue": 204, "alpha": 1.0],
+            "icon": "present.png",
+            "largeIcon": "present_large.png",
+            "backgroundColor": ["red": 28, "green": 171, "blue": 227, "alpha": 1.0],
             "artists": ["Dr. Dre", "Snoop Dogg", "Com Truise", "D12", "Flying Lotus", "Kanye West", "Rundfunk"]
         ],
         [
             "title": "Button Masher",
             "description": "Turn up the speakers and get out of my way! The ultimate gaming playlist to get you hyped up and ready for the crazy fun that’s about to happen.",
-            "icon": "joystick.pdf",
-            "largeIcon": "joystick_large.pdf",
-            "backgroundColor": ["red": 51, "green": 204, "blue": 102, "alpha": 1.0],
+            "icon": "pencils.png",
+            "largeIcon": "pencils_large.png",
+            "backgroundColor": ["red": 255, "green": 252, "blue": 120, "alpha": 1.0],
             "artists": ["Skrillex", "The Game", "2 Chainz", "Jay Z", "T.I.", "Rihanna", "Eminem"]
         ],
         [
             "title": "Futbal Remix",
             "description": "There’s nothing like the world’s game. Kick around the field with this eclectic playlist from around the world. Futbal for life.",
-            "icon": "ball.pdf",
-            "largeIcon": "ball_large.pdf",
-            "backgroundColor": ["red": 255, "green": 102, "blue": 153, "alpha": 1.0],
+            "icon": "trophy.png",
+            "largeIcon": "trophy_large.png",
+            "backgroundColor": ["red": 173, "green": 117, "blue": 197, "alpha": 1.0],
             "artists": ["Shakira", "Santana", "Wyclef Jean", "Aloe Blacc", "Pitbull", "Enrique Iglesias", "Ricky Martin"]
         ]
     ]
